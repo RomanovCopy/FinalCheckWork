4bb215a5d70903941c0d36e26996417df2b9a258
RomanovCopy <RomanovCopy@gmail.com>
2023-03-08 21:45:13 +0300

    Добавлена программа для решения задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write( "Каково колличество вводимых строк? : " );
+int n = Convert.ToInt32( Console.ReadLine() );//колличество строк в массиве
+string[] array = new string[n];
+int count = 0;
+while(count < n)//ввод строк и занесение их в массив
+{
+    Console.Clear();
+    Console.WriteLine( $"Ввод строки {count + 1} из {n}" );
+    array[count] = Console.ReadLine();
+    count = count + 1;
+}
+string[] result = new string[n];
+count = 0;
+int res = 0;
+while (count < n)
+{
+    if (array[count].Length <= 3)
+    {
+        result[res] = array[count];
+        res = res + 1;
+    }
+    count = count + 1;
+}
+Console.WriteLine($"Строки длиной меньше или равной 3 символам: \n{string.Join('\n', result)}" );
+Console.ReadKey();
+
